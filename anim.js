0b8b4c93f59254ef5056bf51737b99c0ba410c7e
VelazquezEspinoza <168579267+VelazquezEspinoza@users.noreply.github.com>
2025-01-28 22:49:27 -0700

    Add files via upload

--- anim.js
+++ anim.js
@@ -3,38 +3,6 @@ var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
-var lyricsData = [
-  { text: "Al fin llegamos", time: 17 },
-  { text: "A la sima del cielo por amarnos", time: 5 },
-  { text: "Al mundo desafiamos", time: 3 },
-  { text: "Seguimos unidos y enamorados", time: 5 },
-  { text: "Yo se…… que nadie…..", time: 4 },
-  { text: "Pensó que nuestro amor duraria", time: 6 },
-  { text: "Pero hooy………", time: 3 },
-  { text: "Aquí estamos amándonos", time: 6 },
-  { text: "Tu y yo", time: 4 },
-  { text: "Sigues siendo tu miii", time: 5 },
-  { text: "Amor eterno yo te amo cada día más", time: 8 },
-  { text: "Tu y yo", time: 4 },
-  { text: "Sigues siendo tu miii", time: 5 },
-  { text: "Sueño eterno y yo el dueño de tu corazón", time: 8 },
-  { text: "Tan solo por mirarte", time: 5 },
-  { text: "Se estremecen los hilos de mi alma", time: 6 },
-  { text: "Tan solo por amarte", time: 5 },
-  { text: "Daria mi vida en un instante", time: 6 },
-  { text: "Yo se…… que nadie…..", time: 5 },
-  { text: "Pensó que nuestro amor duraria", time: 6 },
-  { text: "Pero hooy………", time: 3 },
-  { text: "Aquí estamos amándonos", time: 6 },
-  { text: "Tu y yo", time: 4 },
-  { text: "Sigues siendo tu miii", time: 5 },
-  { text: "Amor eterno yo te amo cada día más", time: 8 },
-  { text: "Tu y yo", time: 4 },
-  { text: "Sigues siendo tu miii", time: 5 },
-  { text: "Sueño eterno y yo el dueño de tu corazón", time: 8 },
-  { text: "Al fin llegamos", time: 4 },
-  { text: "A la sima del cielo por amarnos.", time: 7 },
-];
 
 // Animar las letras
 function updateLyrics() {
